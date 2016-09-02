d146f7c97aadea2842299c0d55b2c13eef6e0555
Giuseppe Acito <giuseppe.acito@bancaditalia.it>
2016-09-02 14:21:25 +0200

    Major overhaul

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,9 @@
 #!/usr/bin/env Rscript
 
-#' @import testthat XML
-NULL
-
 library(testthat)
 library(methods)
 library(crayon)
+
 #' Test reporter: summary of errors in jUnit XML format.
 #'
 #' This reporter includes detailed results about each test and summaries,
@@ -146,7 +144,108 @@ JUnitReporter <- setRefClass("JUnitReporter", contains = "Reporter",
   )
 )
 
-if(require(XML)) {
+JunitReporter <- R6::R6Class(
+  "JunitReporter", inherit = Reporter,
+  public = list(
+    file = NULL,
+    results = NULL,
+    timer = NULL,
+    
+    initialize = function(file = "report.xml") {
+      super$initialize()
+      self$file <- file
+      self$results <- list()
+    },
+    
+    start_reporter = function() {
+      self$timer <- proc.time()
+    },
+    
+    start_context = function(context) {
+      self$cat(context, ": ")
+    },
+    
+    end_context = function(context) {
+      self$cat("\n")
+    },
+    
+    add_result = function(context, test, result) {
+      if (expectation_broken(result)) {
+        self$cat_tight(single_letter_summary(result))
+      }else {
+        self$cat_tight(colourise(".", "success"))
+      }
+      
+      result$time <- round((proc.time() - self$timer)[["elapsed"]], 2)
+      self$timer  <- proc.time()
+      result$test <- if (is.null(test) || test == "") "(unnamed)" else test
+      # call can sometimes contain a second item, "succeed()"
+      result$call <- if (is.null(result$call)) "(unexpected)" else format(result$call)[1]
+      self$results[[context]] <- append(self$results[[context]], list(result))
+    },
+    
+    end_reporter = function() {
+      self$cat("\n")
+      classnameOK <- function(text) {
+        gsub("[ \\.]", "_", text)
+      }
+      # --- suites ---
+      suites <- lapply(names(self$results), function(context) {
+        result <- list <- self$results[[context]]
+        xnames <- vapply(result_list, `[[`, character(1), "call")
+        xnames <- make.unique(xnames, sep = "_")
+        for (i in seq_along(result_list)) {
+          result <- list[[i]]$call <- xnames[[i]]
+        }
+        testcases <- lapply(result_list, function(result) {
+          failnode <- NULL
+          if (expectation_broken(result)) {
+            ref <- result$srcref
+            if ( is.null(ref) ) {
+              location <- ''
+            } else {
+              location <- paste0('(@', attr(ref, 'srcfile')$filename, '#', ref[1], ')')
+            }
+            failnode <-
+              xml <- new_node("failure", attrs =
+                                           c(type = ifelse(result$error, "error", "failure"),
+                                             message = location),
+                              text = as.character(result))
+          }
+          xml <- new_node("testcase", attrs =
+                                        c(classname = paste(classnameOK(context),
+                                                            classnameOK(result$test), sep = "."),
+                                          name = result$success_msg,
+                                          time = result$time,
+                                          message = result$success_msg),
+                          children = if (expectation_broken(result)) list(failnode))
+        }) # testcases
+        ispass <- vapply(result_list, expectation_success, logical(1))
+        iserr <- vapply(result_list, expectation_error,  logical(1))
+        tests <- vapply(result_list, `[[`, character(1), "test")
+        xml <- new_node("testsuite", attrs =
+                                       c(tests = length(result_list),
+                                         failures = sum(!ispass & !iserr),
+                                         errors = sum(iserr),
+                                         name = context,
+                                         time = sum(vapply(result_list, `[[`, numeric(1), "time")),
+                                         timestamp = toString(Sys.time()),
+                                         hostname = Sys.info()[["nodename"]]),
+                        children = testcases)
+      }) # suites
+      
+      # create the final document
+      xmlDoc <- xml_new_document()
+      lapply(suites, function(suite)xml <- add_child(xmlDoc, suite))
+      
+      # this causes a segfault write_xml(xmlDoc, self$file, format = )
+      cat(toString(xmlDoc), file = self$file)
+      
+    } # end_reporter
+  )
+)
+
+if(require(xml2)) {
   file.xml <- file.path(
     Sys.getenv("WORKSPACE", "."), "tests",
     paste0(Sys.getenv("BUILD_NUMBER", "/tests"), ".xml"))
