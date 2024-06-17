792624e2de4797151fb8ac48dc7618ca08feef86
Giuseppe Acito <giuseppe.acito@gmail.com>
2024-06-17 15:18:41 +0200

    updated prev_quarter to a decent implementation

--- R/prev_quarter.r
+++ R/prev_quarter.r
@@ -1,3 +1,14 @@
+#' Return the current quarter
+#'
+#' @param date the date of the quarter
+#' @return the numeric repr of quarter
+
+quarter <- function(date = Sys.Date()) {
+  as.numeric(substr(base::quarters(date), 2, 2))
+}
+
+
+
 #' this function returns the previous quarter compared on the current date
 #'
 #' @param date date to evaluate previous year
@@ -5,39 +16,14 @@
 #' @export
 
 prev_quarter <- function(date = Sys.Date()) {
-  day_of_the_year <- lubridate::yday(date)
-  leap <- ifelse(lubridate::leap_year(date), 1, 0)
-
-  first_quarter_day <- lubridate::yday(as.Date("1977-03-01")) + leap
-  second_quarter_day <- lubridate::yday(as.Date("2013-06-30")) + leap
-  third_quarter_day <- lubridate::yday(as.Date("2021-09-30")) + leap
-  fourth_quarter_day <- lubridate::yday(as.Date("2023-12-31")) + leap
-
   year <- lubridate::year(date)
+  period <- quarter(date = date)
 
-  if (day_of_the_year <= first_quarter_day) {
-    year <- year - 1
-    period <- 3
-
-  } else if (day_of_the_year > first_quarter_day &&
-
-    day_of_the_year <= second_quarter_day) {
-
+  if (period == 1) {
     year <- year - 1
     period <- 4
-
-  } else if (day_of_the_year > second_quarter_day &&
-
-    day_of_the_year <= third_quarter_day) {
-    period <- 1
-
-  } else if (day_of_the_year > third_quarter_day &&
-
-    day_of_the_year <= fourth_quarter_day) {
-    period <- 2
-
   } else {
-    stop("Should never happen")
+    period <- period - 1
   }
 
   c(year, period)
--- tests/testthat/test.prev_quarter.r
+++ tests/testthat/test.prev_quarter.r
@@ -1,23 +1,9 @@
 
 testthat::test_that("prev_quarter does what it claims", {
-  skip_if_not(requireNamespace("mockery"), "mockery required")
-  sys_date_mock <- mockery::mock(
-    as.Date("2020-01-01"),
-    as.Date("2020-02-15"),
-    as.Date("2020-03-31"),
-    as.Date("2020-04-01"),
-    as.Date("2020-09-01"),
-    as.Date("2020-12-01"),
-  )
-
-  mockery::stub(prev_quarter, "Sys.Date", sys_date_mock)
-
-  expect_equal(prev_quarter(), c(2019, 3))
-  expect_equal(prev_quarter(), c(2019, 3))
-  expect_equal(prev_quarter(), c(2019, 4))
-  expect_equal(prev_quarter(), c(2019, 4))
-  expect_equal(prev_quarter(), c(2020, 1))
-  expect_equal(prev_quarter(), c(2020, 2))
-
-  mockery::expect_called(sys_date_mock, 6)
+  expect_equal(prev_quarter(as.Date("2020-01-01")), c(2019, 4))
+  expect_equal(prev_quarter(as.Date("2020-02-15")), c(2019, 4))
+  expect_equal(prev_quarter(as.Date("2020-03-31")), c(2019, 4))
+  expect_equal(prev_quarter(as.Date("2020-04-01")), c(2020, 1))
+  expect_equal(prev_quarter(as.Date("2020-09-01")), c(2020, 2))
+  expect_equal(prev_quarter(as.Date("2020-12-01")), c(2020, 3))
 })
