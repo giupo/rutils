a9d1c6b95cc94909687bf484fef4e61018237e0b
Giuseppe Acito <giuseppe.acito@gmail.com>
2024-10-10 18:11:09 +0200

    Fix: dev: forgot to export gen_unique_filename

--- R/gen_unique_file.r
+++ R/gen_unique_file.r
@@ -2,16 +2,35 @@ create_random_chars <- function(n, values = letters) {
   paste0(sample(values, n, TRUE), collapse = "")
 }
 
-gen_unique_filename <- function(n = 7, prefix = tempdir(), ext = NA) {
+
+#' Generates a unique file name in a directory `prefix`
+#'
+#' it's safe to call this in a multithreaded environment
+#'
+#' @md
+#' @param n number of character per file
+#' @param prefix directory where the file must be created
+#' @param ext filename extension
+#' @param values available values for random filenamem creation
+#'    defaults to `letters`
+#' @return the path where it's safe to create a unique filename
+#' @export
+
+gen_unique_filename <- function(
+  n = 7,
+  prefix = tempdir(),
+  ext = NA,
+  values = letters
+) {
   dir.create(prefix, showWarnings = FALSE, recursive = TRUE)
 
   # lock file management
-  lock_file <- file.path(prefix, ".lock")
+  lock_file <- file.path(prefix, ".gen_unique_filename.lock")
   lock <- filelock::lock(lock_file)
   on.exit(filelock::unlock(lock))
 
   generate_file_name <- function(n, ext = NA) {
-    random_name <- create_random_chars(n)
+    random_name <- create_random_chars(n, values = values)
 
     filename <- if (is.na(ext)) random_name
     else glue::glue("{random_name}.{ext}")
