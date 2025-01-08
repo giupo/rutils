2a67305763640492396e89ad19b5874101dde046
Giuseppe Acito <giuseppe.acito@gmail.com>
2025-01-08 12:44:49 +0100

    Makes a more thoughtful test for is_r_project

--- tests/testthat/test-is_r_project.r
+++ tests/testthat/test-is_r_project.r
@@ -1,11 +1,34 @@
 test_that("is_r_project yields TRUE in an R project", {
-  # NOTE: testthat changes wd to tests/testthat
-  #  that's why we go up two levels
-  expect_true(is_r_project("../.."))
+  path <- tempdir()
+  file.create(file.path(path, c("DESCRIPTION", "NAMESPACE")))
+  dir.create(file.path(path, "R"))
+
+  withr::defer(unlink(path, recursive = TRUE, force = TRUE))
+  expect_true(is_r_project(path))
 })
 
 test_that("is_r_project yields FALSE in any other dir", {
   path <- tempdir()
   withr::defer(unlink(path))
+  expect_false(is_r_project(path))
+})
+
+
+test_that("is_r_project yields FALSE if a single condition is broken", {
+  path <- tempdir()
+  file.create(file.path(path, c("DESCRIPTION")))
+  dir.create(file.path(path, "R"))
+  withr::defer(unlink(path, recursive = TRUE, force = TRUE))
+  expect_false(is_r_project(path))
+
+  path <- tempdir()
+  file.create(file.path(path, c("NAMESPACE")))
+  dir.create(file.path(path, "R"))
+  withr::defer(unlink(path, recursive = TRUE, force = TRUE))
+  expect_false(is_r_project(path))
+
+  path <- tempdir()
+  file.create(file.path(path, c("NAMESPACE", "DESCRIPTION")))
+  withr::defer(unlink(path, recursive = TRUE, force = TRUE))
   expect_false(is_r_project(path))
 })
